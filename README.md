4de2300564c5b03af2781f38f34bb9d318f398f7
alexung3r <alexunger210@gmail.com>
2024-05-20 23:46:33 +0200

    Prepared the files for reproducibility

--- README.md
+++ README.md
@@ -5,7 +5,18 @@ Testing reproducibility of a ranger function on Mac.
 The following items are attached:
 * rf_prepare.R, which is a data preparation file that just has the be sourced in the model_one.R script
 * model_one.R, is the script where the ranger function is set to be run with the seed
-* kleintest.Rproj, is the project I ran the scripts on my personal device
-* housing.csv, the dataset for the random forest
+* check.Rproj, is the project I ran the scripts on my personal device
+* housing.csv, the dataset 
 
 
+## Packages that are needed
+*tidyverse
+*dplyr
+*reshape2
+*ranger
+*dplyr
+*ggplot2
+
+##These were my results
+*OOB prediction error : 4806113672
+*R squared (OOB) : 0.6444524
--- model_one.R
+++ model_one.R
@@ -3,9 +3,7 @@ source("rf_prepare.R")
 
 #load the packages we need 
 #library(kernelshap) 
-library(treeshap) #to apply the tree SHAP algorithm
 library(ranger) #for the random forest model
-library(shapviz) #for the shap visualisation
 library(dplyr) #to do some data engineering
 library(ggplot2) # for plotting
 #library(pdp) # to visualize partial dependence plots
@@ -43,51 +41,8 @@ original_model <- ranger(
 #check performance (OOB error and so on)
 print(original_model)
 
-# check feature importance
-importance_ranger = original_model$variable.importance
-
-# look at the values
-print(importance_ranger)
-
-
-
-
-####to get a more nuanced view lets plot the feature importance
-# Create a data set for the plot
-importance_df <- data.frame(Feature = names(importance_ranger), Importance = importance_ranger)
-
-# Plotting 
-png("Pfi.png", width = 1600, height = 1200, res = 150)
-ggplot(importance_df, aes(x = reorder(Feature, Importance), y = Importance)) +
-  geom_col() +
-  coord_flip() +  # Flip to make it horizontal
-  labs(title = "Feature Importance in Random Forest Model",
-       x = "Features",
-       y = "Importance") +
-  theme_minimal()
-dev.off()
-#median income clearly the most important feature
 
 
 
 
 
-######
-#Test data
-#predictions for test data
-testPred = predict(original_model, data = test)
-
-#compare with the actual values
-actuals <- test$median_house_value
-predictions <- testPred$predictions
-
-# calculate MSE
-mse_rf = mean((predictions - actuals)^2)
-
-# calculate rmse
-test_rmse = sqrt(mse_rf)
-test_rmse
-
-# Correct calculation of MAE
-#MAE = mean(abs(predictions - actuals))
-#MAE
